b7dc886e3683ba789c5a3c2539ed97999500b285
Patrick Huck <phuck@lbl.gov>
2017-07-18 23:35:52 -0700

    dtu/swf: static url

--- swf/explorer/views.py
+++ swf/explorer/views.py
@@ -6,8 +6,8 @@ from django.template import RequestContext
 from mpcontribs.rest.views import get_endpoint
 from mpcontribs.io.core.components import render_dataframe
 from mpcontribs.io.core.recdict import render_dict
-from test_site.settings import STATIC_URL
-from ..rest.rester import SwfRester
+from materials_django.settings import STATIC_URL
+from ..rest.rester import SWFRester
 
 def index(request):
     ctx = RequestContext(request)
